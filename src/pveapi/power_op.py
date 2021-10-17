56067c9cacd4b55a72a7a4d462aceb5b61daa322
muse-sisay <git.muse.sisay@gmail.com>
2021-10-17 09:54:15 +0000

    refactor(power_op.py): change proxmoxer api endpoint calls
    Change to using 'proxmox.get('nodes/%s/openvz' % node['node'])' format.

--- src/pveapi/power_op.py
+++ src/pveapi/power_op.py
@@ -1,43 +1,57 @@
 from proxmoxer.core import ResourceException
 
 
-def get_status (node, vmID):
+def get_status (proxmox , node, vmID):
     '''
     Get the status of running vm
     '''
     try :
-        msg= node.qemu(vmID).status.current.get()['status']
+        msg= proxmox.get(f"nodes/{node}/qemu/{vmID}/status/current")['status']
         return {"msg" : msg, "success" : 1 }
     except ResourceException as e :
         return {"msg" : e  , "success" : 0 }
 
-def power_on(node, vmID):
+def power_on(proxmox ,node, vmID):
     '''
     Power on vm
+    POST /api2/json//nodes/{node}/qemu/{vmid}/status/start
     '''
-    status = get_status(node, vmID)
-    if get_status(node, vmID) == "running" :
-        msg = node.qemu(vmID).status.start.post()
-        return {"msg" : msg, "success" : 1 }
+    status = get_status(proxmox, node, vmID)
+    if status['success'] == 1 :
+        if status['msg'] == "stopped" :
+            msg= proxmox.post(f"nodes/{node}/qemu/{vmID}/status/start")
+            return {"msg" : msg, "success" : 1 }
+        else :
+            return {"msg": "VM already running", "success" : 0 }
     else :
-        return {"success" : 0 }
+        return {"msg" : status['msg'], "success": 0}
 
-def power_off(node, vmID) :
+def power_off(proxmox ,node, vmID) :
     '''
     Power off vm
+    POST /api2/json//nodes/{node}/qemu/{vmid}/status/shutdown
     '''
-    if get_status(node, vmID) == "running" :
-        msg = node.qemu(vmID).status.stop.post()
-        return {"msg" : msg, "success" : 1 }
+    status = get_status(proxmox, node, vmID)
+    if status['success'] == 1 :
+        if status['msg'] == "running" :
+            msg= proxmox.post(f"nodes/{node}/qemu/{vmID}/status/shutdown")
+            return {"msg" : msg, "success" : 1 }
+        else :
+            return {"msg": "VM already running", "success" : 0 }
     else :
-        return {"success" : 0 }
+        return {"msg" : status['msg'], "success": 0}
 
-def reboot( node, vmID):
+def reboot(proxmox ,node, vmID):
     '''
     Reboot vm
+    POST /api2/json//nodes/{node}/qemu/{vmid}/status/reboot
     '''
-    if get_status(node, vmID) == "running" :
-        msg = node.qemu(vmID).status.reboot.post()
-        return {"msg" : msg, "success" : 1 }
+    status = get_status(proxmox, node, vmID)
+    if status['success'] == 1 :
+        if status['msg'] == "running" :
+            msg= proxmox.post(f"nodes/{node}/qemu/{vmID}/status/reboot")
+            return {"msg" : msg, "success" : 1 }
+        else :
+            return {"msg": "VM already running", "success" : 0 }
     else :
-        return {"success" : 0 }
+        return {"msg" : status['msg'], "success": 0}
