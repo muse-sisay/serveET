e23a9ff2076fafd8599bc82bf1bd7a5af69524e5
muse-sisay <git.muse.sisay@gmail.com>
2021-10-23 04:43:44 +0000

    fix: use token for proxmox authentication

--- src/pveapi/__init__.py
+++ src/pveapi/__init__.py
@@ -5,4 +5,5 @@ from util import CONFIG, parser
 proxomox_cred = CONFIG['proxmox']
 
 proxmox = ProxmoxAPI(proxomox_cred['host'], user=proxomox_cred['user'],
-                     password=proxomox_cred['password'], verify_ssl=False)
+                    token_name=proxomox_cred['token']['name'],  
+                    token_value=proxomox_cred['token']['value'], verify_ssl=False)
